23fc7c901d27207174d29e0bd0c442db95741d64
Renske <vluuks@gmail.com>
2023-01-12 14:51:48 +0100

    backing property experiments

--- app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -1,6 +1,20 @@
 package com.example.android.unscramble.ui
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class GameViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(GameUiState())
+    private val _testeroni = "Hoi"
+
+    // Game UI state // Backing property to avoid state updates from other classes
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+    // is direct assignment shorthand for get()?
+    // or does that only work because it's a flow hence the changing parts are within it, but the encapsulating thing stays constant and can be assigned just once?
+
+    val testeroni: String
+        get() = _testeroni
 }
