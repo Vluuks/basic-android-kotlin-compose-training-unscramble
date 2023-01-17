6f91f8a8c89140624f267c2634ece3df266d53eb
Renske <vluuks@gmail.com>
2023-01-17 12:40:47 +0100

    add init/reset method for words

--- app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.android.unscramble.ui
 
 import androidx.lifecycle.ViewModel
+import com.example.android.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -8,6 +9,10 @@ import kotlinx.coroutines.flow.asStateFlow
 class GameViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(GameUiState())
     private val _testeroni = "Hoi"
+
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
 
     // Game UI state // Backing property to avoid state updates from other classes
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
@@ -17,4 +22,36 @@ class GameViewModel : ViewModel() {
 
     val testeroni: String
         get() = _testeroni
+
+    init {
+        resetGame()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+
+    }
+
+    private fun resetGame() {
+        usedWords.clear()
+        currentWord = ""
+    }
+
+    private fun greeting(): String = "hallo"
+    private fun greetingsList(): List<String> = listOf("test", "123")
+
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = (allWords - usedWords).random()
+        usedWords.add(currentWord)
+        return shuffleCurrentWord(currentWord)
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        // Scramble the word
+        tempWord.shuffle()
+        while (String(tempWord).equals(word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+
 }
