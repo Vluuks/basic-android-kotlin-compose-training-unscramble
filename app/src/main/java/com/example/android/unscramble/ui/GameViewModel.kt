ca04f22034a6c1e84a89163f8ccfba5f9b950643
Renske <vluuks@gmail.com>
2023-01-17 14:43:16 +0100

    start with passing data to the composables

--- app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -8,11 +8,9 @@ import kotlinx.coroutines.flow.asStateFlow
 
 class GameViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(GameUiState())
-    private val _testeroni = "Hoi"
 
     private lateinit var currentWord: String
     private var usedWords: MutableSet<String> = mutableSetOf()
-
 
     // Game UI state // Backing property to avoid state updates from other classes
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
@@ -20,22 +18,15 @@ class GameViewModel : ViewModel() {
     // is direct assignment shorthand for get()?
     // or does that only work because it's a flow hence the changing parts are within it, but the encapsulating thing stays constant and can be assigned just once?
 
-    val testeroni: String
-        get() = _testeroni
-
     init {
         resetGame()
         _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
-
     }
 
     private fun resetGame() {
         usedWords.clear()
         currentWord = ""
     }
-
-    private fun greeting(): String = "hallo"
-    private fun greetingsList(): List<String> = listOf("test", "123")
 
     private fun pickRandomWordAndShuffle(): String {
         currentWord = (allWords - usedWords).random()
